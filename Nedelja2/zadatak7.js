05d64c5155964125246c561497432922d10d9f03
Luka Stojilkovic <luka311094@gmail.com>
2020-02-07 17:21:16 +0100

    crtanje

---
+++ Nedelja2/zadatak7.js
@@ -0,0 +1,6 @@
+n=5;
+
+for(i=0;i<=n;i++) {
+    let r='*'.repeat(i);
+    console.log(r);
+}
---
+++ Nedelja2/zadatak8.js
@@ -0,0 +1,6 @@
+n=5;
+
+for(i=n;i>0;i--) {
+    let re=' '.repeat(i-1) + '*'.repeat(n-i+1);
+    console.log(re);
+}
